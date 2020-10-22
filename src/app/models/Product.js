b152ecbf0a207b97736ebacf75109c656c2738c3
Luiz Henrique Rosa <rosaluizhenrique@gmail.com>
2020-10-22 18:04:02 -0300

    Criação dos Schemas/Models

---
+++ src/app/models/Product.js
@@ -0,0 +1,51 @@
+import Mongoose, { Schema } from "mongoose";
+
+const Product = new Schema(
+  {
+    _idRestaurant: { type: Schema.ObjectId, ref: "Restaurant", index: true },
+    name: { type: String, required: true, unique: false },
+    price: { type: Number, required: true },
+    dirPhoto: {
+      type: String,
+      required: true,
+    },
+    categories: {
+      type: [String],
+      required: true,
+    },
+    sales: {
+      _id: false,
+      type: [
+        {
+          salePrice: Number,
+          weekday: String,
+          schedules: [{ _id: false, start: String, end: String }],
+        },
+      ],
+      required: false,
+    },
+  },
+  {
+    timestamps: false,
+    toJSON: { virtuals: true },
+  }
+);
+
+Product.virtual("urlPhoto").get(function () {
+  const path = this.dirPhoto.slice(
+    this.dirPhoto.indexOf("storage"),
+    this.dirPhoto.length
+  );
+  return `${process.env.API_EXTERNAL_URI}/${path}`;
+});
+
+Product.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
+  transform(doc, ret) {
+    delete ret.dirPhoto;
+    delete ret.id;
+  },
+});
+
+export default Mongoose.model("Product", Product);
---
+++ src/app/models/Restaurant.js
@@ -0,0 +1,51 @@
+import Mongoose, { Schema } from "mongoose";
+
+const Restaurant = new Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    adressStreet: { type: String, required: true },
+    adressNumber: { type: String, required: true },
+    adressDistrict: { type: String, required: true },
+    adressCity: { type: String, required: true },
+    adressState: { type: String, required: true },
+    adressCountry: { type: String, required: true },
+    adressZipCode: { type: String, required: true },
+    adressObs: { type: String },
+    openingHours: {
+      _id: false,
+      type: [
+        {
+          weekday: String,
+          schedules: [{ _id: false, start: String, end: String }],
+        },
+      ],
+      required: true,
+    },
+    dirPhoto: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+Restaurant.virtual("urlPhoto").get(function () {
+  const path = this.dirPhoto.slice(
+    this.dirPhoto.indexOf("storage"),
+    this.dirPhoto.length
+  );
+  return `${process.env.API_EXTERNAL_URI}/${path}`;
+});
+
+Restaurant.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
+  transform(doc, ret) {
+    delete ret.dirPhoto;
+    delete ret.id;
+  },
+});
+
+export default Mongoose.model("Restaurant", Restaurant);
